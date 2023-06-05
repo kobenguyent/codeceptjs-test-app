4f9f0406f3bae9c6ea262160ffc3520473dcbf4f
kobenguyent <kobenguyent@gmail.com>
2023-06-05 15:20:17 +0200

    fix: config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/codeceptjs-test-app/',
+  assetsInclude: ['**/*.png'],
+  build: {
+    target: 'esnext'
+  },
 })
